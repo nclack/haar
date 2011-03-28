6e16b2201db40a6221d1058ab5f1aa4b41cc3a0f
Nathan Clack <clackn@janelia.hhmi.org>
2011-03-28 19:22:54 -0400

    Haar: added padding to haar apps
     - timed for 256x512x256 stack:
       10.2 s user time on my mac pro (3 GHz core2 duo, 8GB 1067 MHz DDR3 RAM)
    
       - There seems to be a bug in Release mode.   Not sure whether this is on the
         mylib side or not.  Compiler optimization seems to shave off at most 1 second.
    
       - I profiled the Debug build, and the vast majority of the time is spent
         in the swap operation.  An L2 cache miss profile shows 90% of the
         cache misses are in the swap_op.
    
         - non-in-place could avoid this, but requires some extra memory.
         - looks like a z-order'd recursive implimentation could speed things
           up

--- app/ihaar.c
+++ app/ihaar.c
@@ -2,15 +2,26 @@
 #include <image.h>
 #include <utilities.h>
 #include <haar-mylib.h>
-
+#include <fft.h>       //for Padding routines
 
 char *spec[] = {"<out:TIFF> <in:TIFF>",0};
+
+void pad(Array *a)
+{
+  Coordinate *s=Array_Shape(a);
+  Dimn_Type i,*d;
+  d = (Dimn_Type*) s->data;
+  for(i=0;i<s->size;++i)
+    d[i] = Power_Of_2_Pad(d[i]);
+  Pad_Array_Inplace(a, Idx2CoordA(a, 0), s);
+}
 
 int main(int argc, char *argv[])
 { Process_Arguments(argc, argv, spec, 1);
   { Array *a;
     a = Read_Image(Get_String_Arg("in"), 0);
-    Convert_Array_Inplace(a, PLAIN_KIND, FLOAT32_TYPE, 32);
+    Convert_Array_Inplace(a, PLAIN_KIND, FLOAT32_TYPE, 32, 1.0);
+    pad(a);
     Haar_Inverse_Transform(a);
     Write_Image(Get_String_Arg("out"), a, 0);
     Free_Array(a);
